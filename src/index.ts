be515055b3a69a430b44ed91f04f19c2eb2e04fd
afeiship <1290657123@qq.com>
2021-05-21 22:28:17 +0800

    feat: response type

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import httpSchema from '@jswork/http-schema';
 import nxFetchWithDebug from '@jswork/next-fetch-with-debug';
 import nxFetchWithTimeout from '@jswork/next-fetch-with-timeout';
 import nxFetchWithCancelable from '@jswork/next-fetch-with-cancelable';
+import nxFetchWithResponseType from '@jswork/next-fetch-with-response-type';
 import nxApplyFetchMilddeware from '@jswork/next-apply-fetch-middleware';
 
 export default (inConfig, inOptions?) => {
   const fetch = nxApplyFetchMilddeware([
     nxFetchWithTimeout,
     nxFetchWithCancelable,
+    nxFetchWithResponseType,
     nxFetchWithDebug
   ])(global.fetch);
 
