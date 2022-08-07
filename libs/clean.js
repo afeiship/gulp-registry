9535f8123b75a089b9643b29f447928206b038cd
afeiship <1290657123@qq.com>
2022-08-08 07:48:15 +0800

    fix: del without array wrap

--- libs/clean.js
+++ libs/clean.js
@@ -11,7 +11,7 @@ module.exports = class extends DefaultRegistry {
   init(taker) {
     const buildDir = this.options.buildDir;
     taker.task('clean', function () {
-      return del([buildDir]);
+      return del(buildDir);
     });
   }
 };
