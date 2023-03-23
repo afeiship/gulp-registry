c547ab5885029df2c7464e0f0950a3dd2292ede6
afeiship <1290657123@qq.com>
2023-03-23 16:13:53 +0800

    fix: tsconfig path err

--- libs/ts/scripts.js
+++ libs/ts/scripts.js
@@ -1,7 +1,13 @@
 const DefaultRegistry = require('undertaker-registry');
 const checkModules = require('@jswork/check-modules');
 const defaults = { src: 'src/**/*.ts', dst: './dist' };
-const requiredModules = ['@jswork/gulp-pkg-header', 'gulp-replace', 'gulp-rename', 'gulp-typescript'];
+const path = require('path');
+const requiredModules = [
+  '@jswork/gulp-pkg-header',
+  'gulp-replace',
+  'gulp-rename',
+  'gulp-typescript',
+];
 
 module.exports = class extends DefaultRegistry {
   constructor(inOptions) {
@@ -17,9 +23,10 @@ module.exports = class extends DefaultRegistry {
     const replace = require('gulp-replace');
     const rename = require('gulp-rename');
     const gulpTs = require('gulp-typescript');
-    const tsconfig = require('./tsconfig.json');
+    const tsconfig = require(path.join(process.cwd(), 'tsconfig.json'));
     const opts = tsconfig.compilerOptions;
 
+    // cjs for nodejs
     taker.task('scripts:cjs', function () {
       return taker
         .src(src)
@@ -29,6 +36,7 @@ module.exports = class extends DefaultRegistry {
         .pipe(taker.dest(dst));
     });
 
+    // esm for browser
     taker.task('scripts:esm', function () {
       return taker
         .src(src)
@@ -38,6 +46,7 @@ module.exports = class extends DefaultRegistry {
         .pipe(taker.dest(dst));
     });
 
+    // typing for ts
     taker.task('scripts:typing', function () {
       return taker
         .src(src)
@@ -46,6 +55,7 @@ module.exports = class extends DefaultRegistry {
         .pipe(taker.dest(dst));
     });
 
+    // main task
     taker.task('ts:scripts', taker.series('scripts:cjs', 'scripts:esm', 'scripts:typing'));
   }
 };
