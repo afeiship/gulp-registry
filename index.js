284e8d4769a5d9f8774304ef534477ff9e2c56bd
afeiship <1290657123@qq.com>
2023-03-23 09:05:39 +0800

    feat: add nx:scripts

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ module.exports = {
   JsonRegistry: require('./libs/json'),
   ScssRegistry: require('./libs/scss'),
   Typescript: require('./libs/typescript'),
+  NxScripts: require('./libs/nx/scripts'),
 };
---
+++ libs/nx/scripts.js
@@ -0,0 +1,55 @@
+const DefaultRegistry = require('undertaker-registry');
+const checkModules = require('@jswork/check-modules');
+const capitalize = require('capitalize');
+const defaults = { src: 'src/*.js', dst: './dist', name: 'qs', classify: false };
+const requiredModules = [
+  '@jswork/gulp-pkg-header',
+  'gulp-insert',
+  'gulp-uglify',
+  'gulp-prettier',
+  'gulp-replace',
+  'gulp-rename',
+  'gulp-load-plugins',
+  'uglify-save-license',
+];
+
+module.exports = class extends DefaultRegistry {
+  constructor(inOptions) {
+    super(inOptions);
+    this.options = { ...defaults, ...inOptions };
+  }
+
+  init(taker) {
+    const { src, dst, name, classify } = this.options;
+    if (!checkModules(requiredModules)) return Promise.resolve();
+
+    const saveLicense = require('uglify-save-license');
+    const $ = require('gulp-load-plugins')({
+      pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*'],
+    });
+
+    taker.task('nx:scripts.cjs', function () {
+      return taker
+        .src(src)
+        .pipe($.insert.wrap(`(function () {`, '})();'))
+        .pipe($.prettier())
+        .pipe($.jswork.pkgHeader())
+        .pipe(taker.dest(dst))
+        .pipe($.uglify({ output: { comments: saveLicense } }));
+    });
+
+    taker.task('nx:scripts.esm', () => {
+      const expName = classify ? capitalize(name) : name;
+      const exports = `export default nx.${expName};`;
+      return taker
+        .src(src)
+        .pipe($.jswork.pkgHeader())
+        .pipe($.replace('global || this || window', 'global || this'))
+        .pipe($.replace(/if \(typeof module !== 'undefined' && module\.exports.*\s+.*\s+}/, exports))
+        .pipe($.rename({ extname: '.esm.js' }))
+        .pipe(taker.dest(dst));
+    });
+
+    taker.task('nx:scripts', taker.series('nx:scripts.cjs', 'nx:scripts.esm'));
+  }
+};
