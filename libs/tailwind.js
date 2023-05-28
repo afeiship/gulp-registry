0bd6ed692ee22332ed94f6c57f5b11d731c8a9b4
afeiship <1290657123@qq.com>
2023-05-28 16:26:53 +0800

    feat: add tailwind config.js

--- libs/tailwind.js
+++ libs/tailwind.js
@@ -1,7 +1,15 @@
+const path = require('path');
 const DefaultRegistry = require('undertaker-registry');
 const checkModules = require('@jswork/check-modules');
 const defaults = { src: './src/index.scss', dst: '.' };
-const requiredModules = ['gulp-postcss', 'gulp-dart-sass', 'gulp-sass-glob'];
+const requiredModules = [
+  'gulp-postcss',
+  'gulp-dart-sass',
+  'gulp-sass-glob',
+  'postcss',
+  'tailwindcss',
+  'autoprefixer',
+];
 
 module.exports = class extends DefaultRegistry {
   constructor(inOptions) {
@@ -13,15 +21,19 @@ module.exports = class extends DefaultRegistry {
     const { src, dst } = this.options;
     taker.task('tailwind', function () {
       if (!checkModules(requiredModules)) return Promise.resolve();
+      const cwd = process.cwd();
       const postcss = require('gulp-postcss');
       const sass = require('gulp-dart-sass');
       const sassGlob = require('gulp-sass-glob');
+      const tailwindcss = require('tailwindcss');
+      const autoprefixer = require('autoprefixer');
+      const tailwindConfig = path.join(cwd, 'tailwind.config.js');
 
       return taker
         .src(src)
         .pipe(sassGlob())
         .pipe(sass())
-        .pipe(postcss())
+        .pipe(postcss([tailwindcss(tailwindConfig), autoprefixer()]))
         .pipe(taker.dest(dst));
     });
   }
